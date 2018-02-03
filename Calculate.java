b3ef1ea00506f0ec1791ccd8830bec52a22208b4
Artem Zvonilkin <35900184+Artem-Zvonilkin@users.noreply.github.com>
2018-02-03 13:00:55 +0300

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,36 @@
 public class Calculate {
-    public static void main(String[] args) {
+       public static void main(String[] args) throws IOException {
         System.out.println("Calculate...");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        double first = Double.parseDouble(in.readLine());
+        double second = Double.parseDouble(in.readLine());
+        String operator = in.readLine();
+        double rez=0;
+        switch (operator) {
+            case ("+"):
+                rez = first + second;
+                System.out.println("Результат = "+rez);
+                break;
+            case ("-"):
+                rez = first - second;
+                System.out.println("Результат = "+rez);
+                break;
+            case ("*"):
+                rez = first * second;
+                System.out.println("Результат = "+rez);
+                break;
+            case ("/"):
+                if (second == 0) System.out.println("Деление на 0!");
+                else {
+                    rez = first / second;
+                    System.out.println("Результат = " + rez);
+                }
+                break;
+            case ("^"):
+                rez = Math.pow(first, second);
+                System.out.println("Результат = " + rez);
+                break;
+            default: System.out.println("Данного действия нет");
+        }
     }
 }
